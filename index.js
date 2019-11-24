b99893f55852baa8d911d058a7c468eae3760599
Rodrigo Borges <gmrodrigoborges@gmail.com>
2019-11-24 17:52:55 -0300

    alterando cors

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ Deslike
 Comentário
 */
 app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.set('view engine', 'ejs')
 app.set('views', 'src/views')
 
